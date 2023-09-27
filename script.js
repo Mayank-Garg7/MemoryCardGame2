0452f389c039e074451f0a2871e055d11fe6cc3a
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-27 17:44:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
 
-let matched = 0;
-let cardOne, cardTwo;
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
 let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
+
+function initTimer() {
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+}
 
 function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
+    if(!isPlaying) {
+        isPlaying = true;
+        timer = setInterval(initTimer, 1000);
+    }
+    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+        flips++;
+        flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
         if(!cardOne) {
             return cardOne = clickedCard;
@@ -20,17 +41,16 @@ function flipCard({target: clickedCard}) {
 
 function matchCards(img1, img2) {
     if(img1 === img2) {
-        matched++;
-        if(matched == 8) {
-            setTimeout(() => {
-                return shuffleCard();
-            }, 1000);
+        matchedCard++;
+        if(matchedCard == 6 && timeLeft > 0) {
+            return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = "";
         return disableDeck = false;
     }
+
     setTimeout(() => {
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
@@ -45,21 +65,31 @@ function matchCards(img1, img2) {
 }
 
 function shuffleCard() {
-    matched = 0;
-    disableDeck = false;
+    timeLeft = maxTime;
+    flips = matchedCard = 0;
     cardOne = cardTwo = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
+
+    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-    cards.forEach((card, i) => {
+
+    cards.forEach((card, index) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `images/img-${arr[i]}.png`;
+        setTimeout(() => {
+            imgTag.src = `images/img-${arr[index]}.png`;
+        }, 500);
         card.addEventListener("click", flipCard);
     });
 }
 
 shuffleCard();
-    
+
+refreshBtn.addEventListener("click", shuffleCard);
+
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
